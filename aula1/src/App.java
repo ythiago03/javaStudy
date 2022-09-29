08d209b1fea48bc2972dadf8c6569a8406847924
Thiago Henrique <92443688+ythiago03@users.noreply.github.com>
2022-09-29 07:04:05 -0300

    Add files via upload

---
+++ aula1/src/App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+    
+    public static void main(String[] args){
+        
+        int n1 = 6;
+        int n2 = 5;
+        int n3 = 4;
+        int n4 = 8;
+        float m = (n1 + n2 + n3 + n4) / 4;
+        System.out.println("A média dos números é: " + m);
+    }
+}
